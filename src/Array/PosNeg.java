1dc54acd6cf80fd3588d738145763ad134a00d80
Java <muneeraparveen2k5@gmai.com>
2026-09-23 14:51:49 +0530

    java codes

--- src/Array/PosNeg.java
+++ src/Array/PosNeg.java
@@ -1,4 +1,17 @@
 package Array;
-
+import java.util.Scanner;
 public class PosNeg {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }
+        if(n<0){
+            System.out.println("Negative");
+        }
+        if(n==0){
+            System.out.println("Zero");
+        }
+    }
 }
